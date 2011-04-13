65990175181979221b3cbfc62a17964ff59e553c
Giovani Rinaldi <giovani@giovani-desktop.(none)>
2011-04-13 01:18:43 -0300

    Enum types, static tables (rcon and sbox/invsbox) defined, generation of round key for 128bits key working.

--- src/rijndael.cpp
+++ src/rijndael.cpp
@@ -1,1 +1,46 @@
 #include "rijndael.h"
+#include <cstring>
+
+#include <cstdio>
+
+
+void Rijndael::makeKey(unsigned char** key, KeySize ks, BlockSize bs){
+	_exp_key = new unsigned char* [44];	
+	for (int i = 0; i < 44; i++){
+		_exp_key[i] = new unsigned char [4];
+	}
+	for (int i = 0; i < 4; i++){
+		memcpy(_exp_key[i], key[i], 4);
+	}
+	for (int i = 4; i < 44; i++){
+		memcpy(_exp_key[i], _exp_key[i-1], 4);
+		if (i % 4 == 0){
+			rotWord(_exp_key[i]);
+			subWord(_exp_key[i]);
+			_exp_key[i][0] ^= _rcon[i/4];	//xor Rcon
+		}
+		for (int j = 0; j < 4; j++){
+			_exp_key[i][j] ^= _exp_key[i-4][j];
+		}
+	}
+}
+		
+void Rijndael::rotWord(unsigned char* column){
+	unsigned char temp = column[0];
+	column[0] = column[1];
+	column[1] = column[2];
+	column[2] = column[3];
+	column[3] = temp;
+}
+
+void Rijndael::subWord(unsigned char* column){
+	for (int i = 0; i < 4; i++){
+		column[i] = _sbox[column[i]];
+	}
+}
+
+void Rijndael::encrypt(){
+}
+
+void Rijndael::decrypt(){
+}
--- src/yaaes.cpp
+++ src/yaaes.cpp
@@ -1,5 +1,54 @@
 #include <cstdlib>
+#include <cstdio>
+#include <sys/time.h>
+#include <cstring>
+using namespace std;
+
+#include "rijndael.h"
 
 int main (int argc, char *argv[]){
+	Rijndael r;
+	timeval t1, t2;
+/*	char input[1024];
+	char ch[Rijndael::K128/8];
+	unsigned char uch[Rijndael::K128/8];
+	signed char sch[Rijndael::K128/8];*/
+	unsigned char a[5] = "abcd";
+//	if (fgets(input, 16+1, stdin)){
+//		if (sscanf(input, "%s", uch) == 1){
+			//printf("%d\n", a[0]);
+//		}
+//	}
+//	printf("%d\n", a);
+//	gettimeofday(&t1, 0);
+//	r.rotWord(a);
+//	gettimeofday(&t2, 0);
+/*	long mtime, seconds, useconds;
+	seconds = t2.tv_sec - t1.tv_sec;
+	useconds = t2.tv_usec - t1.tv_usec;
+	mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+	printf("%ld microseconds\n", mtime);
+	printf("%ld %ld\n", t2.tv_sec - t1.tv_sec, t2.tv_usec - t1.tv_usec);*/
+//	printf("%d\n", _sbox[0]);
+	unsigned char ck [4][4] = {
+		{ 0x2b, 0x7e, 0x15, 0x16 },
+		{ 0x28, 0xae, 0xd2, 0xa6 },
+		{ 0xab, 0xf7, 0x15, 0x88 },
+		{ 0x09, 0xcf, 0x4f, 0x3c }
+	};
+	unsigned char** ck1 = new unsigned char* [4];
+	for (int i = 0; i < 4; i++){	ck1[i] = new unsigned char[4];	}
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 4; j++){
+			ck1[i][j] = ck[i][j];
+		}
+	}
+	r.makeKey(ck1, Rijndael::K128, Rijndael::B128);	
+	for (int i = 0; i < 44; i++){
+		for (int j = 0; j < 4; j++){
+			printf("%x ", r._exp_key[i][j]);
+		}
+		printf("\n");
+	}
 	return EXIT_SUCCESS;
 }
